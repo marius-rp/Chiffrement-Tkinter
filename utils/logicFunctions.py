db1fd29d3f47f2bc2c561610273d784ddad73027
Marius <marius.rolland@yahoo.com>
2025-03-31 10:47:39 +0200

    Bug fixes logic functions, add error responses

--- utils/logicFunctions.py
+++ utils/logicFunctions.py
@@ -1,66 +1,57 @@
 import random
 
+from utils.cleanupDisplay import destroy
+
 ### Fonctions ###
 
 """-------------------------
 Ecryption
--------------------------""" 
+-------------------------"""
 
-def encryptionEnd (text, decalage):
+
+def encryptionEnd(text, decalage):
     try:
         result = ""
         for char in text:
-            if char.isupper():
-                result += chr((ord(char) + decalage - 65) % 26 + 65)
-            elif char.islower():
-                result += chr((ord(char) + decalage - 97) % 26 + 97)
+            if char.isalpha():
+                result += chr(ord(char) + decalage)
             else:
                 result += char
-        textEncryption= "Le texte à été chiffré avec succès"
-        print(textEncryption, "green")
         return result
-    except:
-        textErreur= "Erreur dans le chiffrementdu texte"
-        print(textErreur, "red")
+    except Exception as e:
+        print(f"Error in text encryption: {e}")
+        return None
+
 
 def changeSpace(text):
     try:
         caracteres_possibles = ["*", "£", "$"]
-        textModifie = "".join(random.choice(caracteres_possibles) if char == " " else char for char in text)
+        textModifie = "".join(random.choice(
+            caracteres_possibles) if char == " " else char for char in text)
         return textModifie
     except:
-        textErreur = "Erreur dans le chiffrement des espaces"
-        print(textErreur, "red")
+        textError = "Error in the encryption of spaces"
+        print(textError, "red")
 
-def executionEncryption (enter_user, decalage):
-    EncryptionSpace= changeSpace(enter_user)
-    print('changespace: ',EncryptionSpace)
-    EncryptionCaesar = encryptionEnd(EncryptionSpace, decalage)
-    print('encryptioncaesar: ',EncryptionCaesar)
-    EncryptionASCII = hex(int.from_bytes(EncryptionCaesar.encode(), 'big'))
-    print('fin: ',EncryptionASCII)
-    return EncryptionASCII
 
 """-------------------------
 Decryption
--------------------------""" 
+-------------------------"""
+
 
 def decryptionEnd(text, decalage):
     try:
         result = ""
         for char in text:
-            if char.isupper():
-                result += chr((ord(char) - decalage - 65) % 26 + 65)
-            elif char.islower():
-                result += chr((ord(char) - decalage - 97) % 26 + 97)
+            if char.isalpha():
+                result += chr(ord(char) - decalage)
             else:
                 result += char
-        textEncryption = "Le texte à été chiffré avec succès"
-        print(textEncryption)
         return result
-    except:
-        textErreur = "Erreur dans le chiffrementdu texte"
-        print(textErreur, "red")
+    except Exception as e:
+        textError = f"Error in text decryption: {e}"
+        print(textError, "red")
+        return None
 
 
 def addSpace(text):
@@ -70,35 +61,54 @@ def addSpace(text):
             " " if char in caracteresPossibles else char for char in text)
         return textModifie
     except:
-        textErreur = "Erreur dans le chiffrement des espaces"
-        print(textErreur, "red")
+        textError = "Error in the encryption of spaces"
+        print(textError, "red")
+
 
 """-------------------------
 Execution
 -------------------------"""
 
-def executionEncryption (userInput, gap):
-    encryptionSpace= changeSpace(userInput)
-    encryptionCaesar = encryptionEnd(encryptionSpace, gap)
-    encryptionASCII = hex(int.from_bytes(encryptionCaesar.encode(), 'big'))
-    return encryptionASCII
 
-def executionDecryption (userInput, gap):
-    removeUnnecessaryCharacter = userInput[2:]
-    decryptionASCII = bytes.fromhex(removeUnnecessaryCharacter).decode()
-    descryptionCaesar = decryptionEnd(decryptionASCII, gap)
-    descryptionSpace= addSpace(descryptionCaesar)
-    return descryptionSpace
+def executionEncryption(userInput, gap):
+    try:
+        encryptionSpace = changeSpace(userInput)
+        encryptionCaesar = encryptionEnd(encryptionSpace, gap)
+        encryptionASCII = hex(int.from_bytes(encryptionCaesar.encode(), 'big'))
+        return encryptionASCII
+    except Exception as err:
+        textError = f"Error in execution: {err}"
+        print(textError, "red")
 
-def  execution(userInput, gapInput, executionFunction, tk, mainElement, recoveryText):
-    user = userInput.get()
-    gap = int(gapInput.get())
 
-    response = executionFunction(user, gap)
+def executionDecryption(userInput, gap):
+    try:
+        removeUnnecessaryCharacter = userInput[2:]
+        decryptionASCII = bytes.fromhex(removeUnnecessaryCharacter).decode()
+        descryptionCaesar = decryptionEnd(decryptionASCII, gap)
+        descryptionSpace = addSpace(descryptionCaesar)
+        return descryptionSpace
+    except Exception as err:
+        textError = f"Error in decryption: {err}"
+        print(textError, "red")
 
-    labelRecovery = tk.Label(mainElement[1], text=recoveryText, font=(
-        "Times New Roman", 12), bg='#adfeff', fg='black')
-    recovery = tk.Entry(mainElement[1], width=30)
-    recovery.insert(0, response)
-    labelRecovery.pack()
-    recovery.pack()
+
+def execution(userInput, gapInput, executionFunction, tk, responseFrame, recoveryText):
+    try:
+        responseFrame.pack()
+        destroy(responseFrame)
+
+        user = userInput.get()
+        gap = int(gapInput.get())
+
+        response = executionFunction(user, gap)
+
+        labelRecovery = tk.Label(responseFrame, text=recoveryText, font=(
+            "Times New Roman", 12), bg='#adfeff', fg='black')
+        recovery = tk.Entry(responseFrame, width=30)
+        recovery.insert(0, response)
+        labelRecovery.pack()
+        recovery.pack()
+    except:
+        textError = "Error in frame display"
+        print(textError)
